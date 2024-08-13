5d5dab2f7001f28a6a9e1f0b64fabdabcdccdc04
StevenMcgrew <stevenmcgrew8224@msn.com>
2024-08-13 09:29:19 -0700

    File loader functions

--- file_loaders.py
+++ file_loaders.py
@@ -17,3 +17,16 @@ def populate_packages_table(file_name, hash_table):
             status = "at hub"
             package = Package(id_, address, city, state, zip_code, deadline, weight, status)
             hash_table.insert(id_, package)
+
+
+def populate_addresses_list(file_name, addresses_list):
+    with open(file_name) as addresses_file:
+        for line in addresses_file:
+            addresses_list.append(line.strip())
+
+
+def populate_distances_table(file_name, distances_table):
+    with open(file_name) as distances_file:
+        distances = csv.reader(distances_file, delimiter=',')
+        for row in distances:
+            distances_table.append(row)
--- main.py
+++ main.py
@@ -3,15 +3,21 @@
 from package import Package
 from truck import Truck
 from hash_table import HashTable
-from csv_file_loaders import *
+from file_loaders import *
 
 
 def main():
     # Create and populate HashTable for packages
-    packages_table = HashTable()
-    populate_packages_table('packages.csv', packages_table)
+    packages = HashTable()
+    populate_packages_table('packages.csv', packages)
 
-    print(packages_table)
+    # Create and populate List for addresses
+    addresses = []
+    populate_addresses_list('addresses.txt', addresses)
+
+    # Create and populate 2D List for distances
+    distances = []
+    populate_distances_table('distances.csv', distances)
 
 
 if __name__ == '__main__':
